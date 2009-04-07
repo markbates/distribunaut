0ff9b852df6d55acbd98ea10403724fefd71445a
Mark Bates <mark@markbates.com>
2009-04-06 21:23:31 -0400

    Removed some unused errors.

--- lib/distribunaut/errors/errors.rb
+++ lib/distribunaut/errors/errors.rb
@@ -9,14 +9,6 @@ module Distribunaut
           super("APPLICATION: #{app_name} is not a known/registered distributed application.")
         end
       end
-    
-      # Raised when an unknown distributed route name for a distributed application is referenced.
-      class UnknownRouteName < StandardError
-        # Takes the application name and the route name.
-        def initialize(app_name, route_name)
-          super("ROUTE_NAME: #{route_name}, is not a known/registered distributed route name for application: #{app_name}.")
-        end
-      end
       
       # Raised when an application doesn't declare it's application name for use in a distributed system.
       class ApplicationNameUndefined < StandardError
@@ -27,7 +19,8 @@ module Distribunaut
         def inititalize(msg)
           super("Invalid addressable format: #{msg}")
         end
-      end      
+      end
+      
     end # Errors
   end # Distributed
 end # Distribunaut
